83b8c4b0d395ca744d4f7c37f1636edae0818e21
RaflyAdiyasa <rafly271008@gmail.com>
2025-12-25 17:47:12 +0700

    refactor(repository): rename param id to userID

--- internal/domain/repository/user_repository.go
+++ internal/domain/repository/user_repository.go
@@ -6,9 +6,9 @@ import (
 
 type UserRepository interface {
 	Create(user *entity.User) (*entity.User, error)
-	FindByID(id string) (*entity.User, error)
+	FindByID(userID string) (*entity.User, error)
 	FindByEmail(email string) (*entity.User, error)
 	Update(user *entity.User) error
-	Delete(id string) error
+	Delete(userID string) error
 	FindAll() (*[]entity.User, error)
 }
