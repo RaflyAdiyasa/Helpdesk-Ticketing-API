c02f15d4aaeea09cf22517751ed866a830cde5e1
RaflyAdiyasa <rafly271008@gmail.com>
2025-12-25 15:34:11 +0700

    feat(database): add migration

---
+++ internal/infrastructure/database/migration.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"github.com/RaflyAdiyasa/Helpdest-Ticketing-API/internal/domain/entity"
+
+	"gorm.io/gorm"
+)
+
+func RunMigrations(db *gorm.DB) error {
+	err := db.AutoMigrate(
+		&entity.User{},
+		&entity.Ticket{},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
